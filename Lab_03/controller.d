a32f260117adc2d8d8322a049cc3940b93025251
Nester <programmist94@mail.ru>
2018-10-26 09:12:35 +0300

    first_commit

---
+++ Lab_03/controller.d
@@ -0,0 +1,3 @@
+.\controller.o: controller.c
+.\controller.o: controller.h
+.\controller.o: D:\Keil\ARM\Inc\Philips\LPC23xx.H
---
+++ Lab_03/main.d
@@ -0,0 +1,3 @@
+.\main.o: main.c
+.\main.o: D:\Keil\ARM\Inc\Philips\LPC23xx.H
+.\main.o: controller.h
